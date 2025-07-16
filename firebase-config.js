2942df62a5226637e6c46e68dbb988357f0117e9
Abdulwajud <135984022+CYBERDRONE001@users.noreply.github.com>
2025-07-16 10:48:51 +0100

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,1 +1,22 @@
 <!-- firebase-config.js will be replaced with actual implementation -->
+  // Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDGdIsemWvuUTe7Df6iOK7suxMRC8G0vNg",
+  authDomain: "intel-pay-ltd.firebaseapp.com",
+  projectId: "intel-pay-ltd",
+  storageBucket: "intel-pay-ltd.firebasestorage.app",
+  messagingSenderId: "779411397815",
+  appId: "1:779411397815:web:7b6d9f34572d13a833c132",
+  measurementId: "G-6KVXNG7BG3"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
